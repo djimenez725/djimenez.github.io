1d3f05c493c4c27dab3c10c95bd968c0a00cbd32
Dianeris Jimenez <dianerisj@gmail.com>
2025-07-30 19:57:22 -0400

    first commit add index

---
+++ index.js
@@ -0,0 +1,4 @@
+// Store a reference to the <h1> in a variable
+const myHeading = document.querySelector(".intro-title");
+// Update the text content of the <h1>
+myHeading.textContent = "Hello world!"; //textContent-Función que establece y devuelve el contenido del texto. 
